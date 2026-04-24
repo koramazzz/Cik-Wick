d13c1160af88ea8a8c71faf8c26e7cd30ffbeddf
koramazzz <koramazzz@gmail.com>
2026-04-24 17:01:50 +0300

    v0.10 Boostable Spatula Added

--- Assets/_GameAssets/Scripts/Gameplay/Player/PlayerController.cs
+++ Assets/_GameAssets/Scripts/Gameplay/Player/PlayerController.cs
@@ -205,5 +205,10 @@ public class PlayerController : MonoBehaviour
     {
         return _isSliding;
     }
+
+    public Rigidbody GetPlayerRigidbody()
+    {
+        return _playerRigidbody;
+    }
     #endregion
 }
